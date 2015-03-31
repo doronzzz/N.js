d7bd97849e83b995f495ff2e656572c654868380
doronzzz <gourdoron@gmail.com>
2015-03-31 17:53:17 +0300

    added "show-errors" flag to enable error handling msgs for all nurego-widgets

--- app/dist/bin.js
+++ app/dist/bin.js
@@ -10253,29 +10253,36 @@ text_loginHTML = '<div>\r\n\twelcome {{=user.name}} - {{=user.last}}\r\n\t<input
 absNuregoView = function (bb, loginTmpl) {
   var absNuregoView = Backbone.View.extend({
     initialize: function (model, customTmpl) {
+      this.showErrors = utils.URLToArray(window.location.href)['show-errors'];
     },
     errorMsgHandler: function (response) {
-      try {
-        var el = this.$el.find('.ajaxErrorMsg');
-        el.find('.txt').text(response.error.message);
-        el.show();
-      } catch (e) {
+      if (this.showErrors && this.showErrors !== 'false') {
+        try {
+          var el = this.$el.find('.ajaxErrorMsg');
+          el.find('.txt').text(response.error.message);
+          el.show();
+        } catch (e) {
+        }
       }
     },
     modelHttpErrorsHandler: function (model, response, options) {
-      try {
-        var el = this.$el.find('.ajaxErrorMsg');
-        el.find('.txt').text(response.responseText);
-        el.show();
-      } catch (e) {
+      if (this.showErrors && this.showErrors !== 'false') {
+        try {
+          var el = this.$el.find('.ajaxErrorMsg');
+          el.find('.txt').text(response.responseText);
+          el.show();
+        } catch (e) {
+        }
       }
     },
     genericHttpErrorsHandler: function (xhr, textStatus, errorThrown) {
-      try {
-        var el = this.$el.find('.ajaxErrorMsg');
-        el.find('.txt').text(errorThrown);
-        el.show();
-      } catch (e) {
+      if (this.showErrors && this.showErrors !== 'false') {
+        try {
+          var el = this.$el.find('.ajaxErrorMsg');
+          el.find('.txt').text(errorThrown);
+          el.show();
+        } catch (e) {
+        }
       }
     }
   });
--- app/src/scripts/components/abstract/abstract.view.js
+++ app/src/scripts/components/abstract/abstract.view.js
@@ -1,30 +1,37 @@
 define(["backbone"],function(bb,loginTmpl){
 		var absNuregoView = Backbone.View.extend({
 		  initialize: function(model,customTmpl){
+		  	this.showErrors = utils.URLToArray(window.location.href)['show-errors'];
 		  }, 
 
 		  errorMsgHandler:function(response){
-		  	try{
-		  		var el = this.$el.find('.ajaxErrorMsg');
-				el.find('.txt').text(response.error.message);
-				el.show();
-		  	}catch(e){}
+		  	if(this.showErrors && this.showErrors !== "false"){
+			  	try{
+			  		var el = this.$el.find('.ajaxErrorMsg');
+					el.find('.txt').text(response.error.message);
+					el.show();
+			  	}catch(e){}
+		  	}
 		  },
 
 		  modelHttpErrorsHandler:function(model,response,options){
-			try{
-				var el = this.$el.find('.ajaxErrorMsg');
-				el.find('.txt').text(response.responseText);
-				el.show();
-		  	}catch(e){}
+		  	if(this.showErrors && this.showErrors !== "false"){
+		  		try{
+					var el = this.$el.find('.ajaxErrorMsg');
+					el.find('.txt').text(response.responseText);
+					el.show();
+			  	}catch(e){}
+		  	}
 		  },
 
 		  genericHttpErrorsHandler:function(xhr,textStatus,errorThrown){
-			try{
-				var el = this.$el.find('.ajaxErrorMsg');
-				el.find('.txt').text(errorThrown);
-				el.show();
-		  	}catch(e){}
+		  	if(this.showErrors && this.showErrors !== "false"){
+				try{
+					var el = this.$el.find('.ajaxErrorMsg');
+					el.find('.txt').text(errorThrown);
+					el.show();
+			  	}catch(e){}
+			}
 		  }
 
 
